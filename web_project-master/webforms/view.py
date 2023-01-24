6e5d6a3a18d8f16d65064072fb0db66680d69383
halimaHussein <hanahussein6155@gmail.com>
2023-01-24 05:04:16 +0300

    commit

--- web_project-master/webforms/view.py
+++ web_project-master/webforms/view.py
@@ -1,29 +1,36 @@
 # to render html webpages
 import random
 from django.http import HttpResponse
+from articles.models import Article
+
+
+
 
 
 def home_view(request):
     " take in a request django sends a request  return html response we pick to return a response"
 
+    name = 'halima'  # hard coded
+    # api call to some rest api with python and python request
+    number = random.randint(10, 1233123)  # pseduo random
+    article_obj = Article.objects.get(id=1)
 
-name = 'halima'  # hard coded
-# api call to some rest api with python and python request
-number = random.randint(10, 1233123)  # pseduo random
+    # from the database
+    # article_name
+    # article_content
+    article_title = article_obj.title
+    article_content = article_obj.content
 
-# from the database
+    # django templates
+    h1_string = f"""
+    <h1 >hello world{article_title}({article_obj.id})!< /h1 >
 
 
-# django templates
-h1_string = f"""
-<h1 >hello {name} - {number}!< /h1 >
+    """
+    p_string = f"""
 
+    <p>hi{article_content}!</p>
+    """
+    html_string = h1_string + p_string
 
-"""
-p_string = f"""
-
-<p>hi {name} - {number}!</p>
-"""
-html_string = h1_string + p_string
-
-# return HttpResponse(html_string)
+    return HttpResponse(html_string)
