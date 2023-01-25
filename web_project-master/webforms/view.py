6ca0e3faec9142dbb10a3aac7d4e5560e3e5a72b
halimaHussein <hanahussein6155@gmail.com>
2023-01-25 06:04:00 +0300

    coomit

--- web_project-master/webforms/view.py
+++ web_project-master/webforms/view.py
@@ -4,33 +4,27 @@ from django.http import HttpResponse
 from articles.models import Article
 
 
-
-
-
 def home_view(request):
     " take in a request django sends a request  return html response we pick to return a response"
 
     name = 'halima'  # hard coded
-    # api call to some rest api with python and python request
-    number = random.randint(10, 1233123)  # pseduo random
-    article_obj = Article.objects.get(id=1)
+    random_id = random.randint(1, 4)
 
     # from the database
-    # article_name
-    # article_content
-    article_title = article_obj.title
-    article_content = article_obj.content
+
+    article_obj = Article.objects.get(id=random_id)
+    context = {
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "context": article_obj.content
+
+    }
 
     # django templates
-    h1_string = f"""
-    <h1 >hello world{article_title}({article_obj.id})!< /h1 >
-
-
-    """
-    p_string = f"""
-
-    <p>hi{article_content}!</p>
-    """
-    html_string = h1_string + p_string
-
+    html_string = """
+    
+    <h1>{title} (id: {id}!</h1>)
+    <p> {content}!</p>
+    
+    """   .format(**context)
     return HttpResponse(html_string)
